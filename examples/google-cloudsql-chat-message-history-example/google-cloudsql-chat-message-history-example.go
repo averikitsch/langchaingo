82558151add5c1546e2025915029980f5e1bb195
Diegoplas <55992816+Diegoplas@users.noreply.github.com>
2025-03-14 12:50:42 -0600

    CloudSQL CMH Readme and Example (#28)
    * added-readme-and-example
    
    * removed-extra-parenthesis
    
    * modified-to-working-url
    
    * modified-to-working-url-cloudsql

--- examples/google-cloudsql-chat-message-history-example/google-cloudsql-chat-message-history-example.go
+++ examples/google-cloudsql-chat-message-history-example/google-cloudsql-chat-message-history-example.go
@@ -6,64 +6,59 @@ import (
 	"log"
 	"os"
 
-	"github.com/tmc/langchaingo/internal/alloydbutil"
+	"github.com/tmc/langchaingo/internal/cloudsqlutil"
 	"github.com/tmc/langchaingo/llms"
-	"github.com/tmc/langchaingo/memory/alloydb"
+	"github.com/tmc/langchaingo/memory/cloudsql"
 )
 
-// getEnvVariables loads the necessary environment variables for the AlloyDB connection
+// getEnvVariables loads the necessary environment variables for the CloudSQL connection
 // and the chat message history creation.
-func getEnvVariables() (string, string, string, string, string, string, string, string, string) {
-	// Requires environment variable ALLOYDB_USERNAME to be set.
-	username := os.Getenv("ALLOYDB_USERNAME")
+func getEnvVariables() (string, string, string, string, string, string, string, string) {
+	// Requires environment variable CLOUDSQL_USERNAME to be set.
+	username := os.Getenv("CLOUDSQL_USERNAME")
 	if username == "" {
-		log.Fatal("environment variable ALLOYDB_USERNAME is empty")
+		log.Fatal("environment variable CLOUDSQL_USERNAME is empty")
 	}
-	// Requires environment variable ALLOYDB_PASSWORD to be set.
-	password := os.Getenv("ALLOYDB_PASSWORD")
+	// Requires environment variable CLOUDSQL_PASSWORD to be set.
+	password := os.Getenv("CLOUDSQL_PASSWORD")
 	if password == "" {
-		log.Fatal("environment variable ALLOYDB_PASSWORD is empty")
+		log.Fatal("environment variable CLOUDSQL_PASSWORD is empty")
 	}
-	// Requires environment variable ALLOYDB_DATABASE to be set.
-	database := os.Getenv("ALLOYDB_DATABASE")
+	// Requires environment variable CLOUDSQL_DATABASE to be set.
+	database := os.Getenv("CLOUDSQL_DATABASE")
 	if database == "" {
-		log.Fatal("environment variable ALLOYDB_DATABASE is empty")
+		log.Fatal("environment variable CLOUDSQL_DATABASE is empty")
 	}
 	// Requires environment variable PROJECT_ID to be set.
 	projectID := os.Getenv("PROJECT_ID")
 	if projectID == "" {
 		log.Fatal("environment variable PROJECT_ID is empty")
 	}
-	// Requires environment variable ALLOYDB_REGION to be set.
-	region := os.Getenv("ALLOYDB_REGION")
+	// Requires environment variable CLOUDSQL_REGION to be set.
+	region := os.Getenv("CLOUDSQL_REGION")
 	if region == "" {
-		log.Fatal("environment variable ALLOYDB_REGION is empty")
+		log.Fatal("environment variable CLOUDSQL_REGION is empty")
 	}
-	// Requires environment variable ALLOYDB_INSTANCE to be set.
-	instance := os.Getenv("ALLOYDB_INSTANCE")
+	// Requires environment variable CLOUDSQL_INSTANCE to be set.
+	instance := os.Getenv("CLOUDSQL_INSTANCE")
 	if instance == "" {
-		log.Fatal("environment variable ALLOYDB_INSTANCE is empty")
+		log.Fatal("environment variable CLOUDSQL_INSTANCE is empty")
 	}
-	// Requires environment variable ALLOYDB_CLUSTER to be set.
-	cluster := os.Getenv("ALLOYDB_CLUSTER")
-	if cluster == "" {
-		log.Fatal("environment variable ALLOYDB_CLUSTER is empty")
-	}
-	// Requires environment variable ALLOYDB_TABLE to be set.
-	tableName := os.Getenv("ALLOYDB_TABLE")
+	// Requires environment variable CLOUDSQL_TABLE to be set.
+	tableName := os.Getenv("CLOUDSQL_TABLE")
 	if tableName == "" {
-		log.Fatal("environment variable ALLOYDB_TABLE is empty")
+		log.Fatal("environment variable CLOUDSQL_TABLE is empty")
 	}
-	// Requires environment variable ALLOYDB_SESSION_ID to be set.
-	sessionID := os.Getenv("ALLOYDB_SESSION_ID")
+	// Requires environment variable CLOUDSQL_SESSION_ID to be set.
+	sessionID := os.Getenv("CLOUDSQL_SESSION_ID")
 	if sessionID == "" {
-		log.Fatal("environment variable ALLOYDB_SESSION_ID is empty")
+		log.Fatal("environment variable CLOUDSQL_SESSION_ID is empty")
 	}
 
-	return username, password, database, projectID, region, instance, cluster, tableName, sessionID
+	return username, password, database, projectID, region, instance, tableName, sessionID
 }
 
-func printMessages(ctx context.Context, cmh alloydb.ChatMessageHistory) {
+func printMessages(ctx context.Context, cmh cloudsql.ChatMessageHistory) {
 	msgs, err := cmh.Messages(ctx)
 	if err != nil {
 		log.Fatal(err)
@@ -75,14 +70,14 @@ func printMessages(ctx context.Context, cmh alloydb.ChatMessageHistory) {
 
 func main() {
 	// Requires that the Environment variables to be set as indicated in the getEnvVariables function.
-	username, password, database, projectID, region, instance, cluster, tableName, sessionID := getEnvVariables()
+	username, password, database, projectID, region, instance, tableName, sessionID := getEnvVariables()
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	pgEngine, err := alloydbutil.NewPostgresEngine(ctx,
-		alloydbutil.WithUser(username),
-		alloydbutil.WithPassword(password),
-		alloydbutil.WithDatabase(database),
-		alloydbutil.WithAlloyDBInstance(projectID, region, cluster, instance),
+	pgEngine, err := cloudsqlutil.NewPostgresEngine(ctx,
+		cloudsqlutil.WithUser(username),
+		cloudsqlutil.WithPassword(password),
+		cloudsqlutil.WithDatabase(database),
+		cloudsqlutil.WithCloudSQLInstance(projectID, region, instance),
 	)
 	if err != nil {
 		log.Fatal(err)
@@ -95,7 +90,7 @@ func main() {
 	}
 
 	// Creates a new Chat Message History
-	cmh, err := alloydb.NewChatMessageHistory(ctx, *pgEngine, tableName, sessionID)
+	cmh, err := cloudsql.NewChatMessageHistory(ctx, *pgEngine, tableName, sessionID)
 	if err != nil {
 		log.Fatal(err)
 	}
